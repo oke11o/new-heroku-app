b33a37af35e0f8a42871d89d0002930dd58acee2
Sergey Bevzenko <sbevzenko@ozon.ru>
2019-04-29 08:46:18 +0300

    Log home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,8 +11,9 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index()
+    public function index(LoggerInterface $logger)
     {
+        $logger->info("Start page");
         return $this->render('home/index.html.twig', [
             'title' => 'Hello new Heroku app',
         ]);
